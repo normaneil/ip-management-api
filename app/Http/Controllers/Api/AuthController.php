57df1a4aa37337968c0ffbb68c059c5af04d446d
Normaneil E. Macutay <normaneil.macutay@gmail.com>
2022-03-26 23:29:24 +0800

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -81,7 +81,7 @@ class AuthController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Unauthorised',
-            ], 200);
+            ], 401);
         } 
     }
 
